8362d64e1828168120f1f1a7dfa53096158161a9
kstolarczyk <kamil.stolarczyk@student.put.poznan.pl>
2018-06-28 07:18:43 +0200

    First commit

---
+++ Ant.cpp
@@ -0,0 +1,18 @@
+//
+// Created by kamil on 26.06.18.
+//
+
+#include "Ant.h"
+
+Ant::Ant(int len, double **graf) {
+    this->graf = graf;
+    this->visited = new bool[len];
+    std::fill(&this->visited[0], &this->visited[len-1], false);
+}
+Ant::~Ant() {
+    delete this->graf;
+    delete [] this->visited;
+}
+void Ant::Run() {
+
+}
