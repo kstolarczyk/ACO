f038eacc689f9c1c91c9272516844362bc1acd84
kstolarczyk <40631832+kstolarczyk@users.noreply.github.com>
2018-07-03 15:36:28 +0200

    Create instances.cpp

---
+++ instances.cpp
@@ -0,0 +1,22 @@
+#include "instances.h"
+
+std::vector<std::string> Split(const std::string &str, const std::regex & reg) {
+  return {std::sregex_token_iterator(str.begin(), str.end(), reg, -1), std::sregex_token_iterator()};
+}
+
+Miasto * miasta(const char & fileName) {
+  std::fstream file(fileName, std::ios::in);
+  std::string tmp;
+  std::getline(file, tmp);
+  std::regex reg("[^\\d]+");
+  int len = atoi(std::regex_replace(tmp, reg, "").c_str());
+  Miasto * miasta = new Miasto[len];
+  for(int i = 0; i < len; i++) {
+    std::getline(file, tmp);
+    auto splitted = Split(tmp, reg);
+    miasta[i].y = atoi((*(splitted.end() - 1)).c_str());
+    miasta[i].x = atoi((*(splitted.end() - 2)).c_str());
+  }
+  file.close();
+  return miasta;
+}
