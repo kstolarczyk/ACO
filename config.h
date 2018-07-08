30a57699bb66a054c6f4c8e29c47c3c03d4f9e7f
root <root@localhost>
2018-07-08 16:46:26 +0200

    config.h

--- config.h
+++ config.h
@@ -4,10 +4,12 @@
 
 #ifndef ACO_CONFIG_H
 #define ACO_CONFIG_H
+#include <string>
 const double START_FEROMON = 0.5; // początkowa wartość feromonów na każdej krawędzi
 const double ALPHA = 1.0; // parametr istotnoEci feromonC3w
 const double BETA = 5.0; // parametr istotnoEci odległości
 const double QF = 100.0; // wartości feromonów pozostawiana po przebyciu trasy przez mrówki
 const double PF = 0.7; // prędkość parowania feromonów
 const int ANT_COUNT = 69;
+std::string F_NAME = "berlin52.txt";
 #endif //ACO_CONFIG_H
